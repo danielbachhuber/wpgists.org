1db5aad233fd8776b89269135f266b49a67a2359
Daniel Bachhuber <d@danielbachhuber.com>
2014-04-15 13:13:05 -0700

    Zurb's Foundation will make styling this much easier

--- content/themes/wpgists/functions.php
+++ content/themes/wpgists/functions.php
@@ -12,6 +12,7 @@ class WP_Gists {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new WP_Gists;
 			self::$instance->require_files();
+			self::$instance->setup_actions();
 		}
 
 		return self::$instance;
@@ -23,6 +24,25 @@ class WP_Gists {
 	private function require_files() {
 
 
+
+	}
+
+	/**
+	 * Set up necessary WordPress actions
+	 */
+	private function setup_actions() {
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'action_wp_enqueue_scripts' ) );
+	}
+
+	/**
+	 * Enqueue our scripts and styles
+	 */
+	public function action_wp_enqueue_scripts() {
+
+		// Foundation
+		wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/lib/foundation/foundation.min.js', array( 'jquery' ), '5.2.2' );
+		wp_enqueue_style( 'foundation', get_stylesheet_directory_uri() . '/lib/foundation/foundation.min.css', false, '5.2.2' );
 
 	}
 
