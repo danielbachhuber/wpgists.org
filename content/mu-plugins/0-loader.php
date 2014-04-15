527c19a636761f1eb2f6b1c51cd5987fc6ead24d
Daniel Bachhuber <d@danielbachhuber.com>
2014-04-15 12:55:49 -0700

    Hook up Timber as a dependency

---
+++ content/mu-plugins/0-loader.php
@@ -0,0 +1,9 @@
+<?php
+
+$mu_plugins_to_load = array(
+	'timber/timber.php',
+	);
+
+foreach( $mu_plugins_to_load as $mu_plugin_to_load ) {
+	require_once dirname( __FILE__ ) . '/' . $mu_plugin_to_load;
+}
